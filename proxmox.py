14507b9e94b87c7b4d8bd477ef2ab4ddf4c08cb6
tdthai <tdthai.hcmus@gmail.com>
2023-03-20 14:56:06 +0700

    add_func_remove

--- proxmox.py
+++ proxmox.py
@@ -12,7 +12,23 @@ proxmox = ProxmoxAPI(
 )
 # Get a list of all virtual machines in the Proxmox cluster
 vms = proxmox.cluster.resources.get(type='vm')
+pools = proxmox.pools.get()
 
+#Hàm lấy tất cả pool in proxmox
+def get_all_pools(pools):
+    arr = []
+    for poolid in pools:
+            pool = poolid['poolid']
+            arr.append(pool)
+    return arr
+# Hàm xóa tất cả pool rỗng trong proxmox
+def delete_all_pool_null():
+    arr = get_all_pools(pools)
+    for poolid in arr:
+        getpoolid = proxmox.pools(poolid).get()
+        if getpoolid['members'] == []:
+            delete_pools = proxmox.pools(poolid).delete()
+            print(delete_pools)
 #Hàm lấy tất cả VM running trong proxmox
 def get_all_name_vm(vms):
     arr = []
@@ -50,4 +66,5 @@ def write_csv_file(data, filename):
             writer.writerow(row)
 #writetofile(get_all_name_vm(vms))
 #writetofile(get_all_vm_name_pool(vms))
-write_csv_file(get_all_vm_name_cpu_mem(vms),'readme.csv')
+#write_csv_file(get_all_vm_name_cpu_mem(vms),'readme.csv')
+#print(delete_all_pool_null())
