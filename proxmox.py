b6b406f75443edd76c717a88014d9e76f9522e94
Tran Do Thanh Hai <57478186+tdt-hai@users.noreply.github.com>
2023-04-24 16:52:01 +0700

    Update proxmox.py

--- proxmox.py
+++ proxmox.py
@@ -44,7 +44,35 @@ def get_all_firewall(nodes):
                     if 'firewall' not in net:
                         arr.append([name,net])
     return arr
-    
+#Hàm lấy tất cả vm running in node and storage
+def get_all_vm_name_pool1():
+    arr = []
+    for vm in proxmox.cluster.resources.get(type='vm'):
+        pool = vm.get('pool')
+        if vm['status'] == 'running' and pool is not None:
+            name = vm['name']
+            pool = vm['pool']
+            node = vm['node']
+            arr.append([name, pool,node])
+    return arr
+def get_all_vm_storage(nodes,vm_name_pool):
+    arr = []
+    for node in nodes:
+        for vmid in proxmox.nodes(node['node']).qemu.get():
+            if vmid['status'] == 'running':
+                config = proxmox.nodes(node['node']).qemu(vmid['vmid']).config.get()
+                if 'net0' in config:
+                    name = config['name']
+                    scsi0 = config.get('scsi0')
+                    scsi1 = config.get('scsi1')
+                    scsi2 = config.get('scsi2')
+                    scsi3 = config.get('scsi3')
+                    scsi4 = config.get('scsi4')
+                    for vm in vm_name_pool:
+                        if vm[0] == name:
+                            if scsi0 is not None or scsi1 is not None or scsi2 is not None or scsi3 is not None or scsi4 is not None:
+                                arr.append([name,vm[1],vm[2], scsi0,scsi1,scsi2,scsi3,scsi4])
+    return arr
 #Hàm lấy tất cả pool in proxmox
 def get_all_pools(pools):
     arr = []
@@ -71,8 +99,8 @@ def get_all_name_vm(vms):
 #Hàm lấy tất cả VM và pool
 def get_all_vm_name_pool(vms):
     arr = []
-    for vm in vms:
-        if vm['status'] == 'running':
+    pool = vm.get('pool')
+        if vm['status'] == 'running' and pool is not None:
             name = vm['name']
             pool = vm['pool']
             arr.append([name, pool])
